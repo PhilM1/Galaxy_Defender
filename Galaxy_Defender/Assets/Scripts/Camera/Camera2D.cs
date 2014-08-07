8749a34d7cb9838d72c96ab0b2682ba13c1c6e16
Juan Winsor <juan.winsor@gmail.com>
2014-08-07 00:34:22 -0400

    committed 2d camera and screenspace sprite scripts.
    created test scene and included test texture to demonstrate.

---
+++ Galaxy_Defender/Assets/Scripts/Camera/Camera2D.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface ICamera2DListener
+{
+	void Camera2DPostUpdate();
+}
+
+public class Camera2D : MonoBehaviour {
+	
+	public float pixelsPerUnit = 100.0f;
+
+	public float zoom = 1.0f;
+
+	public GameObject followTarget = null;
+	public bool followSmoothing = false;
+	public float followSmoothingAmount = 0.01f;
+
+
+
+	private List<ICamera2DListener> m_CameraListeners = new List<ICamera2DListener>();
+
+	public void AddListener(ICamera2DListener listener)
+	{
+		if( true != m_CameraListeners.Contains(listener) )
+		{
+			m_CameraListeners.Add( listener );
+		}
+	}
+
+	public void RemoveListener(ICamera2DListener listener)
+	{
+		m_CameraListeners.Remove( listener );
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Awake()
+	{
+		//initial camera zoom 1:1 pixel/unit ratio
+		camera.orthographicSize = (Screen.height / pixelsPerUnit / 2.0f) * zoom;
+
+		//set origin at bottom left corner
+		camera.transform.position = new Vector3((Screen.width / pixelsPerUnit / 2.0f) * zoom, camera.orthographicSize, -10);
+	}
+
+	public Rect GetScreenRect()
+	{
+		float top = camera.transform.localPosition.y - (Screen.height / pixelsPerUnit * 0.5f);
+
+		Rect rect = new Rect( 0, top, 0, 0 );
+		return rect;
+	}
+
+	void Update()
+	{
+		camera.orthographicSize = (Screen.height / pixelsPerUnit / 2.0f) * zoom;
+
+		if(followTarget != null)
+		{
+			Vector3 target = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, -10);
+
+			if(followSmoothing == true)
+			{
+				camera.transform.position = Vector3.Lerp(camera.transform.position, target, Mathf.SmoothStep(0f, 1f, followSmoothingAmount * Time.deltaTime));
+			}
+			else
+			{
+				camera.transform.position = target;
+			}
+		}
+
+		//-- post camera update for listeners
+		for(int i = 0; i < m_CameraListeners.Count; i++)
+		{
+			m_CameraListeners[i].Camera2DPostUpdate();
+		}
+	}
+}
---
+++ Galaxy_Defender/Assets/Scripts/Camera/SpriteScreenPosition.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteScreenPosition : MonoBehaviour , ICamera2DListener
+{
+	public bool updateScale = true;
+
+	private Camera2D m_Camera2D = null;
+
+	private Vector2 m_WorldPosition;
+	public Vector2 screenPosition;
+
+	void OnEnable()
+	{
+		//-- cache the Camera2D object
+		m_Camera2D = Camera.main.gameObject.GetComponent<Camera2D>();
+		m_Camera2D.AddListener( this );
+	}
+
+	void OnDestroy()
+	{
+		m_Camera2D.RemoveListener( this );
+	}
+
+	public void SetPosition( Vector2 position )
+	{
+		//-- use the overloaded (float, float) function
+		SetPosition( position.x, position.y );
+	}
+
+	public void SetPosition( float x, float y )
+	{
+		//-- convert desired screen space coordinates to world coordinates
+		if( m_Camera2D != null )
+		{
+			//-- cache the screen space coordinates
+			screenPosition.x = x;
+			screenPosition.y = y;
+
+			//-- convert screen space to world space
+			float wY = ( Camera.main.transform.localPosition.y + Camera.main.orthographicSize ) - ( y / m_Camera2D.pixelsPerUnit * m_Camera2D.zoom );
+			float wX = ( Camera.main.transform.localPosition.x - (Camera.main.orthographicSize * Camera.main.aspect) ) + ( x / m_Camera2D.pixelsPerUnit * m_Camera2D.zoom );
+
+			//-- cache the world space coordinates
+			m_WorldPosition = new Vector2(wX, wY);
+			//-- ste the new world space coordinates
+			transform.localPosition = m_WorldPosition;
+		}
+		else
+		{
+			//-- can't do calculations withtout Camera2D values
+			throw new UnityException("Camera2D not found");
+		}
+	}
+
+	private void UpdateScale()
+	{
+		//-- scale with the cameras zoom
+		if(m_Camera2D != null)
+		{
+			Vector3 scale = new Vector3(m_Camera2D.zoom, m_Camera2D.zoom, 1.0f);
+			transform.localScale = scale;
+		}
+	}
+
+	void LateUpdate()
+	{
+
+	}
+
+	public void Camera2DPostUpdate()
+	{
+		//-- update the scale with the camera zoom change
+		if(true == updateScale)
+		{
+			UpdateScale();
+		}
+		
+		//-- set the world position by screenspace coordinates
+		SetPosition(screenPosition);
+	}
+
+}
